e7a6f2075711639c7a3043d18711c782b734b988
aaron <alkrauss48@gmail.com>
2014-09-05 13:41:59 -0500

    Clean up main and environment file

--- babylon-world-demo/js/environment.js
+++ babylon-world-demo/js/environment.js
@@ -1,52 +1,53 @@
 ﻿function createScene(engine) {
-    //Creation of the scene
-    var canvas = document.getElementById("renderCanvas");
-    var scene = new BABYLON.Scene(engine);
-
-    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(10, 50, 50), scene);
-    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 1.2, 30, new BABYLON.Vector3(-10,10,0), scene);
-
-    // Define Skybox
-    var skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
-    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-    skyboxMaterial.backFaceCulling = false;
-    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("images/skybox/skybox", scene);
-    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-    skybox.material = skyboxMaterial;
-
-    // Define main ground - with height map
-    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "images/heightMap.png", 100, 100, 100, 0, 10, scene, false);
-    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
-    groundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
-    groundMaterial.diffuseTexture.uScale = 6;
-    groundMaterial.diffuseTexture.vScale = 6;
-    groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-    ground.position.y = -2.0;
-    ground.material = groundMaterial;
+  //Creation of the scene
+  var canvas  = document.getElementById("renderCanvas");
+  var scene   = new BABYLON.Scene(engine);
+  var light   = new BABYLON.PointLight("Omni", new BABYLON.Vector3(10, 50, 50), scene);
+  var camera  = new BABYLON.ArcRotateCamera("Camera", 0, 1.2, 30, new BABYLON.Vector3(-10,10,0), scene);
 
 
-    // Define extra ground to cover entire viewport
-    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
-    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
-    extraGroundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
-    extraGroundMaterial.diffuseTexture.uScale = 60;
-    extraGroundMaterial.diffuseTexture.vScale = 60;
-    extraGround.position.y = -2.05;
-    extraGround.material = extraGroundMaterial;
+  // Define Skybox
+  var skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
+  var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+  skyboxMaterial.backFaceCulling = false;
+  skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("images/skybox/skybox", scene);
+  skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+  skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+  skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+  skybox.material = skyboxMaterial;
 
-    // Water - covers entire viewport
-    // WaterMaterial object defined in waterMaterial.js
-    BABYLON.Engine.ShadersRepository = "";
-    var water = BABYLON.Mesh.CreateGround("water", 1000, 1000, 1, scene, false);
-    var waterMaterial = new WaterMaterial("water", scene, light);
 
-    waterMaterial.refractionTexture.renderList.push(extraGround);
-    waterMaterial.refractionTexture.renderList.push(ground);
-    waterMaterial.reflectionTexture.renderList.push(skybox);
+  // Define main ground - with height map
+  var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "images/heightMap.png", 100, 100, 100, 0, 10, scene, false);
+  var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+  groundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
+  groundMaterial.diffuseTexture.uScale = 6;
+  groundMaterial.diffuseTexture.vScale = 6;
+  groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+  ground.position.y = -2.0;
+  ground.material = groundMaterial;
 
-    water.material = waterMaterial;
 
-    return scene;
+  // Define extra ground to cover entire viewport
+  // Similar to above, but no height map, and much larger
+  var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
+  var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
+  extraGroundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
+  extraGroundMaterial.diffuseTexture.uScale = 60;
+  extraGroundMaterial.diffuseTexture.vScale = 60;
+  extraGround.position.y = -2.05;
+  extraGround.material = extraGroundMaterial;
+
+
+  // Water - covers entire viewport
+  // WaterMaterial object defined in waterMaterial.js
+  BABYLON.Engine.ShadersRepository = "";
+  var water = BABYLON.Mesh.CreateGround("water", 1000, 1000, 1, scene, false);
+  var waterMaterial = new WaterMaterial("water", scene, light);
+  waterMaterial.refractionTexture.renderList.push(extraGround);
+  waterMaterial.refractionTexture.renderList.push(ground);
+  waterMaterial.reflectionTexture.renderList.push(skybox);
+  water.material = waterMaterial;
+
+  return scene;
 }
--- babylon-world-demo/js/main.js
+++ babylon-world-demo/js/main.js
@@ -1,27 +1,26 @@
 ﻿window.onload = function(){
-    var canvas = document.getElementById("canvas");
+  var canvas = document.getElementById("canvas");
 
-    // Check support
-    if (!BABYLON.Engine.isSupported()) {
-        window.alert('Browser not supported');
-    } else {
-        // Babylon
-        var engine = new BABYLON.Engine(canvas, true);
+  // Check support
+  if (!BABYLON.Engine.isSupported()) {
+    window.alert('Browser not supported');
+  } else {
+    // Babylon
+    var engine = new BABYLON.Engine(canvas, true);
 
-        //Creating scene
-        var scene = createScene(engine);
+    //Creating scene
+    var scene = createScene(engine);
 
-        scene.activeCamera.attachControl(canvas);
+    scene.activeCamera.attachControl(canvas);
 
-        // Once the scene is loaded, just register a render loop to render it
-        engine.runRenderLoop(function () {
-            scene.render();
-        });
+    // Once the scene is loaded, we register a render loop to render it
+    engine.runRenderLoop(function () {
+      scene.render();
+    });
 
-        // Resize
-        window.addEventListener("resize", function () {
-            engine.resize();
-        });
-    } 
-
+    // Resize
+    window.addEventListener("resize", function () {
+      engine.resize();
+    });
+  }
 };

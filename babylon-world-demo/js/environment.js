d401be2f720314d5290b87fe4c161cd99f89ffd5
aaron <alkrauss48@gmail.com>
2014-09-05 13:32:12 -0500

    Create vendor lib for babylon.js and hand.js

--- babylon-world-demo/js/environment.js
+++ babylon-world-demo/js/environment.js
@@ -1,7 +1,6 @@
 ﻿function createSceneTuto(engine) {
     //Creation of the scene
     var canvas = document.getElementById("renderCanvas");
-    // var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
 
     var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(10, 50, 50), scene);
@@ -17,15 +16,7 @@
     skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
     skybox.material = skyboxMaterial;
 
-    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
-    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
-    extraGroundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
-    extraGroundMaterial.diffuseTexture.uScale = 60;
-    extraGroundMaterial.diffuseTexture.vScale = 60;
-    extraGround.position.y = -2.05;
-    extraGround.material = extraGroundMaterial;
-
-    // Define ground - with height map
+    // Define main ground - with height map
     var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "images/heightMap.png", 100, 100, 100, 0, 10, scene, false);
     var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
     groundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
@@ -35,18 +26,27 @@
     ground.position.y = -2.0;
     ground.material = groundMaterial;
 
-    // Water
+
+    // Define extra ground to cover entire viewport
+    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
+    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
+    extraGroundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
+    extraGroundMaterial.diffuseTexture.uScale = 60;
+    extraGroundMaterial.diffuseTexture.vScale = 60;
+    extraGround.position.y = -2.05;
+    extraGround.material = extraGroundMaterial;
+
+    // Water - covers entire viewport
+    // WaterMaterial object defined in waterMaterial.js
     BABYLON.Engine.ShadersRepository = "";
     var water = BABYLON.Mesh.CreateGround("water", 1000, 1000, 1, scene, false);
     var waterMaterial = new WaterMaterial("water", scene, light);
+
     waterMaterial.refractionTexture.renderList.push(extraGround);
     waterMaterial.refractionTexture.renderList.push(ground);
-
-    waterMaterial.reflectionTexture.renderList.push(ground);
     waterMaterial.reflectionTexture.renderList.push(skybox);
 
     water.material = waterMaterial;
 
     return scene;
-
 }

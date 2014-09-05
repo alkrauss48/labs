67e796dcca1911fa348b6979b30f15a716ca679a
aaron <alkrauss48@gmail.com>
2014-09-05 07:17:16 -0500

    Add babylon demo

---
+++ babylon-demo/environment.js
@@ -0,0 +1,52 @@
+﻿function createSceneTuto(engine) {
+    //Creation of the scene
+    var canvas = document.getElementById("renderCanvas");
+    // var engine = new BABYLON.Engine(canvas, true);
+    var scene = new BABYLON.Scene(engine);
+
+    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(10, 50, 50), scene);
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 1.2, 30, new BABYLON.Vector3(-10,10,0), scene);
+
+    // Define Skybox
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+
+    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
+    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
+    extraGroundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
+    extraGroundMaterial.diffuseTexture.uScale = 60;
+    extraGroundMaterial.diffuseTexture.vScale = 60;
+    extraGround.position.y = -2.05;
+    extraGround.material = extraGroundMaterial;
+
+    // Define ground - with height map
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "images/heightMap.png", 100, 100, 100, 0, 10, scene, false);
+    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("images/ground.jpg", scene);
+    groundMaterial.diffuseTexture.uScale = 6;
+    groundMaterial.diffuseTexture.vScale = 6;
+    groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    ground.position.y = -2.0;
+    ground.material = groundMaterial;
+
+    // Water
+    BABYLON.Engine.ShadersRepository = "";
+    var water = BABYLON.Mesh.CreateGround("water", 1000, 1000, 1, scene, false);
+    var waterMaterial = new WaterMaterial("water", scene, light);
+    waterMaterial.refractionTexture.renderList.push(extraGround);
+    waterMaterial.refractionTexture.renderList.push(ground);
+
+    waterMaterial.reflectionTexture.renderList.push(ground);
+    waterMaterial.reflectionTexture.renderList.push(skybox);
+
+    water.material = waterMaterial;
+
+    return scene;
+
+}

72ee3a3c46f6d8c5b259c3836241a5c70bc6eb10
aaron <alkrauss48@gmail.com>
2014-09-09 14:57:17 -0500

    add more boxes button, and smoothen increase/decrease size

--- masonry-demo/src/js/app.js
+++ masonry-demo/src/js/app.js
@@ -9,60 +9,77 @@ require('./lib/console-support.js');
 
 // Tracks outbound-link activity through Google Analytics
 require('./lib/outbound.js');
+// add columnWidth function to Masonry
 
-$(document).ready(function(){
+$(function() {
+  $(document).ready(function(){
 
-  var transitionProp = getStyleProperty('transition');
-  var transitionEndEvent = {
-    WebkitTransition: 'webkitTransitionEnd',
-    MozTransition: 'transitionend',
-    OTransition: 'otransitionend',
-    transition: 'transitionend'
-  }[ transitionProp ];
+    var transitionProp = getStyleProperty('transition');
+    var transitionEndEvent = {
+      WebkitTransition: 'webkitTransitionEnd',
+      MozTransition: 'transitionend',
+      OTransition: 'otransitionend',
+      transition: 'transitionend'
+    }[ transitionProp ];
 
-  $container = $('.wrapper').masonry({
-    columnWidth: ".grid-sizer",
-    itemSelector: ".item"
-  });
+    var color = { 0: 'red', 1: 'blue', 2: 'yellow', 3: 'green' }
 
-  $container.on( 'click', '.item-content', function( event ) {
-    var $this = $(this);
-    var previousContentSize = getSize( this );
-    // disable transition
-    this.style[ transitionProp ] = 'none';
-    // set current size
-    $this.css({
-      width: previousContentSize.width,
-      height: previousContentSize.height
+    $container = $('.wrapper').masonry({
+      columnWidth: ".grid-sizer",
+      itemSelector: ".item"
     });
 
-    // $(this).parent().siblings().removeClass('giant');
-    var $itemElem = $this.parent().toggleClass('giant');
-
-    // force redraw
-    var redraw = this.offsetWidth;
-
-    // renable default transition
-    this.style[ transitionProp ] = '';
-
-    // reset 100%/100% sizing after transition end
-    if ( transitionProp ) {
-      var _this = this;
-      var onTransitionEnd = function() {
-        _this.style.width = '';
-        _this.style.height = '';
-      };
-      $this.one( transitionEndEvent, onTransitionEnd );
-    }
-
-    // set new size
-    var size = getSize( $itemElem[0] );
-    $this.css({
-      width: size.width,
-      height: size.height
+    $('a.more-boxes').click(function(event){
+      event.preventDefault();
+      for(var x = 0; x < 4; x++){
+        var div = jQuery('<div/>', { class: 'item' });
+        div.append('<div class="item-content ' + color[x] + '"></div>');
+        $container.append(div);
+        $container.masonry( 'addItems', $('.item:last') )
+      }
+      $container.masonry()
     });
 
-    $container.masonry();
 
+    $container.on( 'click', '.item-content', function( event ) {
+      var $this = $(this);
+      var previousContentSize = getSize( this );
+      // disable transition
+      this.style[ transitionProp ] = 'none';
+      // set current size
+      $this.css({
+        width: previousContentSize.width,
+        height: previousContentSize.height
+      });
+
+      // $(this).parent().siblings().removeClass('giant');
+      var $itemElem = $this.parent().toggleClass('giant');
+
+      // force redraw
+      var redraw = this.offsetWidth;
+
+      // renable default transition
+      this.style[ transitionProp ] = '';
+
+      // reset 100%/100% sizing after transition end
+      if ( transitionProp ) {
+        var _this = this;
+        var onTransitionEnd = function() {
+          _this.style.width = '';
+          _this.style.height = '';
+        };
+        $this.one( transitionEndEvent, onTransitionEnd );
+      }
+
+      // set new size
+      var size = getSize( $itemElem[0] );
+      $this.css({
+        width: size.width,
+        height: size.height
+      });
+
+      $container.masonry();
+
+    });
   });
 });

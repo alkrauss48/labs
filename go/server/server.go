541794837cb20515db32a87844382ce5bce77501
aaron <alkrauss48@gmail.com>
2014-09-15 08:24:20 -0500

    Add path validation to abide by a standard regex

--- go/server/server.go
+++ go/server/server.go
@@ -4,9 +4,13 @@ import (
   "io/ioutil"
   "html/template"
   "net/http"
+  "regexp"
+  "errors"
 )
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 type Page struct {
   Title string
@@ -27,6 +31,15 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+      http.NotFound(w, r)
+      return "", errors.New("Invalid Page Title")
+  }
+  return m[2], nil // The title is the second subexpression.
+}
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
   err := templates.ExecuteTemplate(w, tmpl+".html", p)
   if err != nil {
@@ -35,7 +48,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):] // resliced to drop the "/view/"
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -45,7 +61,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/edit/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   p, err := loadPage(title)
   if err != nil {
       p = &Page{Title: title}
@@ -54,10 +73,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/save/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
-  err := p.save()
+  err = p.save()
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
   }

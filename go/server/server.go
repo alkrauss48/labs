9094a78ab8ecc9402bb5a473d5ca20dbebc542cf
aaron <alkrauss48@gmail.com>
2014-09-14 22:17:06 -0500

    Add main method to create and load test file Create test file

--- go/server/server.go
+++ go/server/server.go
@@ -23,3 +23,10 @@ func loadPage(title string) (*Page, error) {
   }
   return &Page{Title: title, Body: body}, nil
 }
+
+func main() {
+    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+    p1.save()
+    p2, _ := loadPage("TestPage")
+    fmt.Println(string(p2.Body))
+}

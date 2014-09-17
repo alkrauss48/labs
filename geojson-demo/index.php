84d706862d98ee65d58293873bcefd2a1169e7d3
aaron <alkrauss48@gmail.com>
2014-09-17 14:27:44 -0500

    Updated with base tag

--- geojson-demo/index.php
+++ geojson-demo/index.php
@@ -1,57 +1,74 @@
-<div id="map_canvas" style="display: block; height: 100%;"></div>
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 
-<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-<script>
-  // Set basic map attributes
-  var mapOptions = {
-    zoom:   5,
-    center: new google.maps.LatLng(38.16,-98.72),
-    mapTypeControlOptions: {
-      mapTypeIds: ['map_style']
-    }
-  };
+  <title>GeoJson Demo</title>
+  <base href="/geojson-demo/" target="" />
 
-  // Set styles from ./styles.json to a styles var
-  <?php echo('var styles = ' . file_get_contents("./styles.json") . ';'); ?>
+  <style type="text/css" media="all">
+    body, html { height: 100%; }
+  </style>
 
-  var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+</head>
+<body>
+  <div id="map_canvas" style="display: block; height: 100%;"></div>
 
-  // create the map object, and set it to be styled
-  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-  map.mapTypes.set('map_style', styledMap);
-  map.setMapTypeId('map_style');
-
-  // Run through all .geo.json files in ./geojson/, and load them into the map
-  // Check out a lot of GeoJSON files at https://github.com/johan/world.geo.json
-  <?php
-    $dir = new DirectoryIterator('glob://' . dirname(__FILE__) . '/geojson/*.geo.json');
-    foreach ($dir as $fileinfo) {
-      if (!$fileinfo->isDot()) {
-        echo('map.data.loadGeoJson("./geojson/' . $fileinfo->getFilename() . '");');
+  <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+  <script>
+    // Set basic map attributes
+    var mapOptions = {
+      zoom:   5,
+      center: new google.maps.LatLng(38.16,-98.72),
+      mapTypeControlOptions: {
+        mapTypeIds: ['map_style']
       }
-    }
-  ?>
+    };
 
-  // Overlay Styles
-  map.data.setStyle({
-    fillColor: '#2687bf',
-    fillOpacity: .3,
-    strokeWeight: 0
-  });
+    // Set styles from ./styles.json to a styles var
+    <?php echo('var styles = ' . file_get_contents("./styles.json") . ';'); ?>
 
-  // Custom marker using SVG-like paths
-  var star = {
-    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-    fillColor: 'yellow',
-    fillOpacity: .75,
-    scale: .15,
-    strokeWeight: 0
-  };
+    var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
 
-  // Create a marker
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(35.4822, -97.5350),
-    icon:     star,
-    map:      map
-  });
-</script>
+    // create the map object, and set it to be styled
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
+    // Run through all .geo.json files in ./geojson/, and load them into the map
+    // Check out a lot of GeoJSON files at https://github.com/johan/world.geo.json
+    <?php
+      $dir = new DirectoryIterator('glob://' . dirname(__FILE__) . '/geojson/*.geo.json');
+      foreach ($dir as $fileinfo) {
+        if (!$fileinfo->isDot()) {
+          echo('map.data.loadGeoJson("./geojson/' . $fileinfo->getFilename() . '");');
+        }
+      }
+    ?>
+
+    // Overlay Styles
+    map.data.setStyle({
+      fillColor: '#2687bf',
+      fillOpacity: .3,
+      strokeWeight: 0
+    });
+
+    // Custom marker using SVG-like paths
+    var star = {
+      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+      fillColor: 'yellow',
+      fillOpacity: .75,
+      scale: .15,
+      strokeWeight: 0
+    };
+
+    // Create a marker
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(35.4822, -97.5350),
+      icon:     star,
+      map:      map
+    });
+  </script>
+
+</body>
+</html>

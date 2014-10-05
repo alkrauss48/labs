3b128a79540723c9917cf75bd5adadec73af8754
aaron <alkrauss48@gmail.com>
2014-10-04 23:27:01 -0500

    Move html5up site into main index of labs

--- index.php
+++ index.php
@@ -1,14 +1,226 @@
-<h1>Labs!</h1>
+<!DOCTYPE HTML>
+<!--
+  Strongly Typed by HTML5 UP
+  html5up.net | @n33co
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+  <head>
+    <title>Strongly Typed by HTML5 UP</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.dropotron.min.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-layers.min.js"></script>
+    <script src="js/init.js"></script>
+    <noscript>
+      <link rel="stylesheet" href="css/skel.css" />
+      <link rel="stylesheet" href="css/style.css" />
+      <link rel="stylesheet" href="css/style-desktop.css" />
+    </noscript>
+    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+  </head>
+  <body class="homepage">
 
-<ul>
-  <li><a href="geojson-demo">GeoJSON Demo</a></li>
-  <li><a href="babylon-world-demo">BabyjonJS Demo</a></li>
-  <li><a href="d3/subway">D3 Demo</a></li>
-  <li><a href="masonry-demo">MasonryJS Demo</a></li>
-  <li><a href="tea-timer">Tea Timer</a></li>
-  <li><a href="tea-chart">Tea Steep Chart</a></li>
-  <li><a href="carnegie-chart">Dale Carnegie Practice Chart</a></li>
-  <li><a href="http://wiki.thesocietea.org">Go Wiki</a></li>
-</ul>
+    <!-- Header -->
+      <div id="header-wrapper">
+        <div id="header" class="container">
 
-<?php include 'analytics.php'; ?>
+          <!-- Logo -->
+            <h1 id="logo"><a href="index.html">My Labs</a></h1>
+            <p>A repository of fun projects by Aaron Krauss</p>
+
+          <!-- Nav -->
+
+        </div>
+      </div>
+
+    <!-- Features -->
+      <div id="features-wrapper">
+        <section id="features" class="container">
+          <header>
+            <h2>Take a look at what I've been working on</h2>
+          </header>
+          <div class="row">
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="geojson-demo" class="image featured"><img
+                    src="images/google_maps_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>GeoJSON Demo</h3>
+                  </header>
+                  <p>This is a little demo using the <strong>Google Maps
+                    API</strong> and GeoJSON files. Check out how I use them to
+                  color specific geographical regions.</p>
+                </section>
+
+            </div>
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="babylon-world-demo" class="image featured"><img src="images/babylon_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>BabylonJS Demo</h3>
+                  </header>
+                  <p><a href="http://babylonjs.com/" target="blank">BabylonJS</a> is a popular library for
+                  creating 3D environments using <strong>WebGL</strong>. Use first person
+                  shooter controls to move, and spacebar to toggle gravity!</p>
+                </section>
+
+            </div>
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="d3/subway" class="image featured"><img src="images/d3_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>Interactive D3 Demo</h3>
+                  </header>
+                  <p><a href="http://d3js.org/" target="blank">D3.js</a> is
+                  a javascript library for creating SVG graphics based on data
+                  from json documents.</p>
+                </section>
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="masonry-demo" class="image featured"><img
+                    src="images/masonry_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>MasonryJS Demo</h3>
+                  </header>
+                  <p><a href="http://masonry.desandro.com/"
+                    target="blank">MasonryJS</a> is a sweet JS library which
+                  uses intense math behind the scenes to lay out your web page in a dynamic tile
+                  structure.</p>
+                </section>
+
+            </div>
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="tea-timer" class="image featured"><img
+                    src="images/tea_timer_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>Tea Timers</h3>
+                  </header>
+                  <p>I love <strong>Tea</strong>, so I thought I would build
+                  some tea timers to complement my tea chart and help any other
+                  tea enthusiasts with preparing loose-leaf tea.</p>
+                </section>
+
+            </div>
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="tea-chart" class="image featured"><img
+                    src="images/tea_chart_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>Tea Chart</h3>
+                  </header>
+                  <p>This was my first step in the world of Bootstrap, so I
+                  built a tiny page to show off the different tea types and best
+                  steeping practices.</p>
+                </section>
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="carnegie-chart" class="image featured"><img
+                    src="images/carnegie_chart_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>Dale Carnegie Chart</h3>
+                  </header>
+                  <p>For those of you who have read <a
+                    target="blank" href="http://www.amazon.com/How-Win-Friends-Influence-People/dp/0671027034">How to Win
+                    Friends and Influence People</a>, you know how powerful it
+                  is. This chart will help you practice that book.</p>
+                </section>
+
+            </div>
+            <div class="4u">
+
+              <!-- Feature -->
+                <section>
+                  <a href="http://wiki.thesocietea.org" class="image featured"><img
+                    src="images/go_wiki_th.jpg" alt="" /></a>
+                  <header>
+                    <h3>Go Wiki</h3>
+                  </header>
+                  <p>My first real <strong>Go</strong> project! Go Wiki is a
+                  fully functioning wiki server built in Go that is tied to a <strong>MongoDB</strong>
+                  collection to persist data.</p>
+                </section>
+
+            </div>
+          </div>
+          <ul class="actions">
+            <li><a href="https://github.com/alkrauss48/labs" class="button icon fa-file">Check out the GitHub
+              Repo</a></li>
+          </ul>
+        </section>
+      </div>
+
+    <!-- Banner -->
+      <div id="banner-wrapper">
+        <div class="inner">
+          <section id="banner" class="container">
+            <p>Welcome to my laboratory.<br />No beakers. No test tubes. Just code.</p>
+          </section>
+        </div>
+      </div>
+    <!-- Footer -->
+      <div id="footer-wrapper">
+        <div id="footer" class="container">
+          <header>
+            <h2>If you enjoyed these projects, <a target="blank" href="http://thesocietea.org">Check out my main site</a></h2>
+          </header>
+            <div class="12u">
+              <section>
+                <p>I enjoy working on all types of projects, ranging from
+                design-based CMS sites to hardcore back-end apps and APIs. I
+                only post small personal demos and projects here, but if you want to
+                see the cool things I've done at various companies or larger
+                projects, check out my <a target="blank" href="http://thesocietea.org">main
+                site</a> or my <a target="blank" href="https://github.com/alkrauss48">GitHub page</a></p>
+                <div style="text-align: center;" class="row">
+                  <div class="icon 4u fa-envelope">
+                    <a href="mailto:alkrauss48@gmail.com"> alkrauss48@gmail.com</a>
+                  </div>
+                  <div class="icon 4u fa-twitter">
+                    <a target="blank" href="https://twitter.com/thecodeboss"> @thecodeboss</a>
+                  </div>
+                  <div class="icon 4u fa-github">
+                    <a target="blank" href="https://github.com/alkrauss48"> alkrauss48</a>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+        <div id="copyright" class="container">
+          <ul class="links">
+            <li>&copy; <a href="http://thesocietea.org">Aaron Krauss</a>. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+          </ul>
+        </div>
+      </div>
+
+    <?php include 'analytics.php'; ?>
+  </body>
+</html>

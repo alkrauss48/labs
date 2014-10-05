0f9011aadda434e16eb87a38b0d3f195acd910e9
aaron <alkrauss48@gmail.com>
2014-10-04 23:48:59 -0500

    Update labs title

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE HTML>
-<!--
-  Strongly Typed by HTML5 UP
-  html5up.net | @n33co
-  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
   <head>
-    <title>Strongly Typed by HTML5 UP</title>
+    <title>Labs | Aaron Krauss</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="description" content="" />
     <meta name="keywords" content="" />

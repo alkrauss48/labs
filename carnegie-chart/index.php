dc1f92bfce6cc03abea96a7585b2cf5b261dc889
aaron <alkrauss48@gmail.com>
2021-11-04 20:25:25 -0500

    Carnegie Chart: Update OG content Change URLs to labs.thecodeboss.dev

--- carnegie-chart/index.php
+++ carnegie-chart/index.php
@@ -6,15 +6,18 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="description" content="A 30-day chart to help you practice the principles discussed in Dale Carnegie's 'How to Win Friends and Influence People.'">
   <meta property="og:description" content="A 30-day chart to help you practice the principles discussed in Dale Carnegie's 'How to Win Friends and Influence People.'">
-  <meta property="og:image" content="http://www.gravatar.com/avatar/60dcb92b89386677028bb23cd0c73f0e?s=600">
-  <meta property="og:url" content="http://labs.thesocietea.org/carnegie-chart/">
+  <meta property="og:image" content="https://labs.thecodeboss.dev/carnegie-chart/images/carnegie-og-image.jpg">
+  <meta property="og:url" content="https://labs.thecodeboss.dev/carnegie-chart/">
   <meta property="og:title" content="Dale Carnegie 30-Day Chart">
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@thecodeboss" />
+  <meta name="twitter:creator" content="@thecodeboss" />
 
   <base href="/carnegie-chart/" target="" />
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/foundation/4.3.2/css/foundation.min.css" type="text/css" />
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css" type="text/css" />
   <link rel="stylesheet" href="styles/custom.css" type="text/css" />
-  <link rel="shortcut icon" href="favicon.ico" />
+  <link rel="shortcut icon" href="images/favicon.ico" />
   <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js"></script>
 
   <title>Carnegie 30-Day Chart</title>
@@ -24,7 +27,7 @@
   <div class="row">
     <div class="large-12 columns">
       <h1 class="center">Carnegie 30-Day Chart</h1>
-      <h4 class="center">By <a href="https://thesocietea.org" target="blank">Aaron Krauss</a></h4>
+      <h4 class="center">By <a href="https://thecodeboss.dev" target="blank">Aaron Krauss</a></h4>
       <p class="center italic">The Guide to Practicing
         <span class="underline">
           <a target="blank" href="http://www.amazon.com/How-Win-Friends-Influence-People/dp/0671723650">
